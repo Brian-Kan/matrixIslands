c82561e1f8a8c2b702e7361be098af2e6ea4ffb0
Brian Kan <BrianKan00@hotmail.com>
2020-02-10 17:59:19 -0500

    Created a function that is more specific in targeting islands (1).  Still trying to target 'undefined'.

--- script.js
+++ script.js
@@ -37,6 +37,39 @@ const landFinder = function (map) {
     return land.length
 }
 
-let mapLand = landFinder(islandMapTwo)
+const islandFinder = function (map) {
+    const island = []
+    for (i = 0; i < map.length; i++) {
+        for (j = 0; j < map[i].length; j++) {
+            console.log("i is: ", i)
+            console.log("j is: ", j)
+            console.log("map[i][j]:", map[i][j])
+
+            const north = map[i + 1][j]
+            console.log("north is: ", north)
+
+            const south = map[i - 1][j]
+            console.log("south is: ", south)
+
+            const east = map[i][j + 1]
+            console.log("east is: ", east)
+
+            const west = map[i][j - 1]
+            console.log("west is: ", west)
+
+
+            if (map[i][j] === 1 && (north === 0 || undefined) && (south === 0 || undefined) && (west === 0 || undefined) && (east === 0 || undefined)){
+                island.push(1)
+            }
+        }
+    }
+    return island.length
+}
+
+// islandMapOne
+// islandMapTwo
+// landMap
+
+let mapLand = islandFinder(islandMapTwo)
 
 console.log(mapLand)
